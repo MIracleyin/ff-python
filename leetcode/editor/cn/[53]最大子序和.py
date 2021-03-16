920714dd951a3c5e7c90f1c75898309223d82193
Miracleyin <miracleyin@live.com>
2021-03-16 08:51:49 +0800

    leetcode 53

--- leetcode/editor/cn/[53]最大子序和.py
+++ leetcode/editor/cn/[53]最大子序和.py
@@ -61,5 +61,35 @@
 from typing import List
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
+    def maxSubArray_vio(self, nums: List[int]) -> int:
+        pre, maxAns = 0, nums[0]
+        for i in range(len(nums)):
+            pre = max(pre + nums[i], nums[i])
+            maxAns = max(maxAns, pre)
+        return maxAns
+    def maxSubArray_dp(self, nums: List[int]) -> int:
+        n = len(nums)
+        # 递归终止
+        if n == 1:
+            return nums[0]
+        else:
+            max_le = self.maxSubArray_dp(nums[0: len(nums) // 2])
+            max_ri = self.maxSubArray_dp(nums[len(nums) // 2:len(nums)])
+
+        max_l = nums[len(nums) // 2 - 1]
+        tmp = 0
+        for i in range(len(nums) // 2 - 1, -1, -1):
+            tmp += nums[i]
+            max_l = max(tmp, max_l)
+        max_r = nums[len(nums) // 2]
+        tmp = 0
+        for i in range(len(nums) // 2, len(nums)):
+            tmp += nums[i]
+            max_r = max(tmp, max_r)
+        return max(max_le, max_ri, max_l + max_r)
 # leetcode submit region end(Prohibit modification and deletion)
+def main():
+    nums = [-2,1,-3,4,-1,2,1,-5,4]
+    res = Solution().maxSubArray(nums)
+    print(res)
+main()
